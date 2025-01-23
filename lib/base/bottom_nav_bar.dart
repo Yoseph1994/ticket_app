e467d2616228e6a7361f1e9135ebd7d260b0ff73
unknown <joshrde2002@gmail.com>
2025-01-23 14:54:19 +0300

    bottom_navbar and app_bar

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,63 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/screens/home_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final appScreen = [
+    const HomeScreen(),
+    Center(child: Text('Search')),
+    Center(child: Text('Tickets')),
+    Center(child: Text('Profile')),
+  ];
+  var selectedIndex = 0;
+  void _onTappedIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Center(
+            child:
+                Text("Ticket Booking", style: TextStyle(color: Colors.white))),
+      ),
+      body: appScreen[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Color(0xFF526400),
+        showSelectedLabels: false,
+        currentIndex: selectedIndex,
+        onTap: _onTappedIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: 'Tickets'),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
